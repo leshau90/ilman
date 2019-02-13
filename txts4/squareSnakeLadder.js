5230612f01a689320a6fe12620d2ff9ce074c26b
ilman <ilman1990@gmail.com>
2019-02-13 15:49:26 +0700

    tidying no unshift

--- txts4/squareSnakeLadder.js
+++ txts4/squareSnakeLadder.js
@@ -1,11 +1,11 @@
 //with no unshift
 function squareNumber(num) {
     //square numbered array of arrays , snake-ladder like
-    
+
     if (num < 3 || num == undefined) return false;
     var result = [];
     var i = 0;
-    var horizontalIndex = num-1;
+    var horizontalIndex = num - 1;
     var end = num * num;
     while (i < num) {
         result.push(new Array(num));
@@ -14,24 +14,24 @@ function squareNumber(num) {
     i = 1;
     while (i < end + 1) {
         //different direction for every horizontal index
-        if (i!=1 && i%num==1) horizontalIndex--;
-        
+        if (i != 1 && i % num == 1) horizontalIndex--;
+
         //and the direction of filling is affected by whether horizontal index is odd or even  and also by the input number isit odd or even 
-        if (horizontalIndex % 2 == (num%2)) {
+        if (horizontalIndex % 2 == (num % 2)) {
             //from left to right
-            for(let u = 0;u<result[horizontalIndex].length;u++){
-                // --i;
+            for (let u = 0; u < result[horizontalIndex].length; u++) {
+
                 result[horizontalIndex][u] = i++;
             }
-           
+
         } else {
             //from right to left
-            for(let u = result[horizontalIndex].length-1;u>=0;u--){
-                // --i;
+            for (let u = result[horizontalIndex].length - 1; u >= 0; u--) {
+
                 result[horizontalIndex][u] = i++;
             }
             // result[horizontalIndex].push(i);
-            
+
         }
         // i++;
     }
@@ -40,7 +40,7 @@ function squareNumber(num) {
 
 // function squareNumber(num) {
 //     //square numbered array of arrays , snake-ladder like
-    
+
 //     if (num < 3 || num == undefined) return false;
 //     var result = [];
 //     var i = 0;
@@ -57,11 +57,11 @@ function squareNumber(num) {
 
 //         //and the direction of filling is affected by whether horizontal index is odd or even  and also by the input number isit odd or even 
 //         if (horizontalIndex % 2 == (num%2)) {
-            
+
 //             result[horizontalIndex].unshift(i);
 //         } else {
 //             result[horizontalIndex].push(i);
-            
+
 //         }
 //         i++;
 //     }
