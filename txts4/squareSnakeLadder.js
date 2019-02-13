d8f2fdaa6ef533467c908a1bec9c4cf158c57111
ilman <ilman1990@gmail.com>
2019-02-13 17:20:31 +0700

    hmm...

--- txts4/squareSnakeLadder.js
+++ txts4/squareSnakeLadder.js
@@ -14,23 +14,23 @@ function squareNumber(num) {
     i = 1;
     while (i < end + 1) {
         //different direction for every horizontal index
-        if (i != 1 && i % num == 1) horizontalIndex--;
+        if (i != 1 ) horizontalIndex--;
 
         //and the direction of filling is affected by whether horizontal index is odd or even  and also by the input number is it odd or even 
         if (horizontalIndex % 2 == (num % 2)) {
-            //from left to right
+            //from right to left
             for (let u = result[horizontalIndex].length - 1; u >= 0; u--) {
 
                 result[horizontalIndex][u] = i++;
             }
 
         } else {
-            //from right to left
+            //from left to right
             for (let u = 0; u < result[horizontalIndex].length; u++) {
 
                 result[horizontalIndex][u] = i++;
             }
-            
+
             // result[horizontalIndex].push(i);
 
         }
@@ -43,10 +43,8 @@ function squareNumber(num) {
 console.log(squareNumber(3));
 console.log(squareNumber(4));
 console.log(squareNumber(5));
-console.log(squareNumber(6));
-console.log(squareNumber(7));
-console.log(squareNumber(8));
-console.log(squareNumber(9));
+
+console.log(squareNumber(10));
 
 
 // function squareNumber(num) {
@@ -78,4 +76,3 @@ console.log(squareNumber(9));
 //     }
 //     return result;
 // }
-
